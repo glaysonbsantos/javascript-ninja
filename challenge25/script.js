f8e374ac371ac49fdb4f00ece06f7565e05e5b6b
Glayson <glaysonbss@gmail.com>
2022-07-02 00:43:21 -0300

    Challenge 25 solved

---
+++ challenge25/script.js
@@ -0,0 +1,35 @@
+(function (win, doc) {
+  "use strict";
+  /*
+    Essa semana você terá dois desafios:
+    1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
+    tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
+    ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
+    o que não ficou tão claro das aulas anteriores.
+    É essencial que você entenda todo o conteúdo que foi passado até aqui,
+    para que possamos prosseguir para a parte mais avançada do curso :D
+
+    2) Estudar eventos!
+    Acesse a página do MDN:
+    https://developer.mozilla.org/en-US/docs/Web/Events#Categories
+
+    Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
+    desafio os experimentos legais que você conseguir desenvolver :D
+  */
+
+  function on(element, event, callback) {
+    doc.querySelector(element).addEventListener(event, callback, false);
+  }
+
+  function off(element, event, callback) {
+    doc.querySelector(element).removeEventListener(event, callback, false);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    alert("clicou no a");
+  }
+
+  on('[data-js="link"', "click", handleClick);
+  //off('[data-js="link"', "click", handleClick);
+})(window, document);
